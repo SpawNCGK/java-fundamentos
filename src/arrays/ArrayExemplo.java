a4f35183d099209cf62ca2b7f253ef428d3085a4
Augusto Cesar <gutold.cgkever@gmail.com>
2026-03-08 23:14:23 -0300

    Update ArrayExemplo.java

--- src/arrays/ArrayExemplo.java
+++ src/arrays/ArrayExemplo.java
@@ -1,1 +1,15 @@
 
+package arrays;
+
+public class ArrayExemplo {
+
+    public static void main(String[] args) {
+
+        int[] numeros = {10, 20, 30, 40};
+
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+
+    }
+}
