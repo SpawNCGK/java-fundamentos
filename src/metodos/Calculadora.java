bbdb5324fb654065f161b9e85a128f85083174cc
Augusto Cesar <gutold.cgkever@gmail.com>
2026-03-08 23:13:50 -0300

    Update Calculadora.java

--- src/metodos/Calculadora.java
+++ src/metodos/Calculadora.java
@@ -1,1 +1,22 @@
 
+package metodos;
+
+public class Calculadora {
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+
+        int resultado1 = somar(5, 3);
+        int resultado2 = subtrair(10, 4);
+
+        System.out.println("Soma: " + resultado1);
+        System.out.println("Subtração: " + resultado2);
+    }
+}
