c14ffb64ba9fbf3fba0f7511f238c5a740697a93
Augusto Cesar <gutold.cgkever@gmail.com>
2026-03-08 22:37:27 -0300

    Update Variaveis.java

--- src/variaveis/Variaveis.java
+++ src/variaveis/Variaveis.java
@@ -1,1 +1,14 @@
+package variaveis;
 
+public class Variaveis {
+
+    public static void main(String[] args) {
+
+        int idade = 25;
+        String nome = "Augusto";
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+
+    }
+}
